0ea3ed84a6fe95065c86240231ecef2af874ade5
SnehalPatil2003 <snehal2003vision@gmail.com>
2023-09-10 10:47:17 +0530

    third commit

--- hello.java
+++ hello.java
@@ -3,5 +3,8 @@ class a
     public static void main(String ar[])
     {
         System.out.println("hello");
+        System.out.println("sky");
+         System.out.println("How's you?");
+        
     }
 }
